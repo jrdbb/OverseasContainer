0e612747fe5771a371d299b2142b7ca6f4c7a8e7
jinliu <o206@qq.com>
2024-05-29 11:19:35 +0800

    disable s3fs cache

--- s3fs/docker-entrypoint.sh
+++ s3fs/docker-entrypoint.sh
@@ -16,9 +16,9 @@ if [ "$IAM_ROLE" == "none" ]; then
   chmod 0400 /etc/passwd-s3fs
 
   echo 'IAM_ROLE is not set - mounting S3 with credentials from ENV'
-  /usr/bin/s3fs  ${S3_BUCKET} ${MNT_POINT} -d -f -o url=${S3_URL},retries=5,allow_other,umask=0,use_cache=/tmp/s3fs-cache,enable_noobj_cache,sigv2,del_cache,multipart_size=64,max_write=131072,big_writes,kernel_cache,max_background=1000,max_stat_cache_size=100000,parallel_count=30,multireq_max=30
+  /usr/bin/s3fs  ${S3_BUCKET} ${MNT_POINT} -d -f -o url=${S3_URL},retries=5,allow_other,umask=0,enable_noobj_cache,sigv2,del_cache,multipart_size=64,max_write=131072,big_writes,kernel_cache,max_background=1000,max_stat_cache_size=100000,parallel_count=30,multireq_max=30
   echo 'started...'
 else
   echo 'IAM_ROLE is set - using it to mount S3'
-  /usr/bin/s3fs ${S3_BUCKET} ${MNT_POINT} -d -f -o url=${S3_URL},iam_role=${IAM_ROLE},retries=5,allow_other,umask=0,use_cache=/tmp/s3fs-cache,enable_noobj_cache,sigv2,del_cache,multipart_size=64,max_write=131072,big_writes,kernel_cache,max_background=1000,max_stat_cache_size=100000,parallel_count=30,multireq_max=30
+  /usr/bin/s3fs ${S3_BUCKET} ${MNT_POINT} -d -f -o url=${S3_URL},iam_role=${IAM_ROLE},retries=5,allow_other,umask=0,enable_noobj_cache,sigv2,del_cache,multipart_size=64,max_write=131072,big_writes,kernel_cache,max_background=1000,max_stat_cache_size=100000,parallel_count=30,multireq_max=30
 fi
