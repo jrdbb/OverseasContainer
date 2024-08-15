10b45398d009ab0ff8dc91759f82229c4f6dbc9d
jinliu <o206@qq.com>
2024-08-15 09:51:35 +0800

    fix

--- goofys/docker-entrypoint.sh
+++ goofys/docker-entrypoint.sh
@@ -6,7 +6,7 @@ IFS=$'\n\t'
 
 mkdir -p ${MNT_POINT}
 
-export AWS_ACCESS_KEY_ID =${AWS_ACCESS_KEY_ID :-$AWS_KEY}
-export AWS_SECRET_ACCESS_KEY =${AWS_SECRET_ACCESS_KEY :-$AWS_SECRET_KEY}
+export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID:-$AWS_KEY}
+export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY:-$AWS_SECRET_KEY}
 
-/usr/bin/goofys ${S3_BUCKET} ${MNT_POINT} --endpoint ${S3_URL} --subdomain
+/usr/bin/goofys --endpoint ${S3_URL} --subdomain -o allow_other -f --region ${S3_REGION} ${S3_BUCKET} ${MNT_POINT} 
