b4b36e7d48ff284a9ce828d3c237bf79935e9b35
jinliu <o206@qq.com>
2024-08-15 10:12:16 +0800

    fix file mode

--- goofys/docker-entrypoint.sh
+++ goofys/docker-entrypoint.sh
@@ -9,4 +9,4 @@ mkdir -p ${MNT_POINT}
 export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID:-$AWS_KEY}
 export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY:-$AWS_SECRET_KEY}
 
-/usr/bin/goofys --endpoint ${S3_URL} --subdomain -o allow_other -f --region ${S3_REGION} ${S3_BUCKET} ${MNT_POINT} 
+/usr/bin/goofys --endpoint ${S3_URL} --subdomain -o allow_other -f --region ${S3_REGION} --file-mode=0777 --dir-mode=0777 ${S3_BUCKET} ${MNT_POINT} 
